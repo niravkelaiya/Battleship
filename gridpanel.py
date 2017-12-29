64de793ac032707fed9a3e5778459f394e543397
niravkelaiya <niravkelaiya77@gmail.com>
2017-12-29 18:43:02 +0530

    updated grid panel with halt functionality

--- gridpanel.py
+++ gridpanel.py
@@ -2,25 +2,23 @@ from Tkinter import *
 class Grid:
     labels = []
     buttons=[]
+    clicked=[]
     m1=Tk()
     im3 = PhotoImage(file="C:\Users\Crest\Downloads\ship.GIF")
+    label1 = Label(m1, text="Waiting for player's move", font="Times 12 bold", background="yellow")
     ship1=[1,11,21,31]
     def change(self,index):
         self.buttons[index].config(state="disabled")
-        # m2 = Tk()
-        # label1 = Label(m2, text="Waiting for player's move")
-        # label1.configure(background="yellow")
-        # label1.grid(row=5,column=5)
-        # m2.focus_force()
-        self.m1.withdraw()
-       # m1.after(5000)
-       # m2.destroy()
-        self.m1.deiconify()
-        self.putships()
+        self.clicked.append(self.buttons[index])
+        for button in self.buttons:
+            button.config(state="disabled")
 
     def putships(self):
         i = 0
         index = 0
+        for button in self.buttons:
+            if button not in self.clicked:
+                button.config(state="normal")
         for label in self.labels:
             if i==len(self.ship1):
                 break
@@ -31,7 +29,7 @@ class Grid:
     def start(self):
         im1 = PhotoImage(file="C:\Users\Crest\Downloads\shortline.GIF")
         im = PhotoImage(file="C:\Users\Crest\Downloads\labelimg.GIF")
-        self.m1.geometry('1053x470+130+130')
+        #self.m1.geometry('1053x470+130+130')
         indexl = 0
         for i in range(0, 10):
             for j in range(0, 11):
@@ -57,48 +55,10 @@ class Grid:
         head2 = Label(self.m1, text="Board", font="Times 12 bold")
         head3 = Label(self.m1, text="Attack", font="Times 12 bold")
         head4 = Label(self.m1, text="Board", font="Times 12 bold")
+        ready = Button(self.m1, text="Start", font="Times 12 bold",command=lambda:self.putships())
         head1.grid(row=i + 4, column=5)
         head2.grid(row=i + 4, column=6)
         head3.grid(row=i + 4, column=17)
         head4.grid(row=i + 4, column=18)
+        ready.grid(row=i+3,column=11)
         mainloop()
-
-# m1 = Tk()
-# im1 = PhotoImage(file="C:\Users\Crest\Downloads\shortline.GIF")
-# im=PhotoImage(file="C:\Users\Crest\Downloads\labelimg.GIF")
-# im3=PhotoImage(file="C:\Users\Crest\Downloads\ship.GIF")
-# m1.geometry('1053x470+130+130')
-# labels=[]
-# indexl=0
-# for i in range(0,10):
-#     for j in range(0,11):
-#         if j==10:
-#             line = Label(m1)
-#             line.config(image=im1)
-#             line.grid(row=i,column=11)
-#         else:
-#             left = Label(m1)
-#             left.config(image=im)
-#             left.grid(row=i,column=j)
-#             labels.append(left)
-#             indexl+=1
-# index=0
-# buttons=[]
-# for i in range(0,10):
-#     for j in range(14,24):
-#         button = Button(m1,command=lambda index=index:change(index))
-#         button.config(image=im)
-#         button.grid(row=i,column=j)
-#         buttons.append(button)
-#         index+=1
-# head1=Label(m1,text="Your", font="Times 12 bold")
-# head2=Label(m1,text="Board", font="Times 12 bold")
-# head3=Label(m1,text="Attack", font="Times 12 bold")
-# head4=Label(m1,text="Board", font="Times 12 bold")
-# head1.grid(row=i+4,column=5)
-# head2.grid(row=i+4,column=6)
-# head3.grid(row=i+4,column=17)
-# head4.grid(row=i+4,column=18)
-#
-# ship1=[1,11,21,31]
-# mainloop()
